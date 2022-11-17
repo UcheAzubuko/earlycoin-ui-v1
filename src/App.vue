6c6216d01324519f9daef4369f517602599c78fc
Uche Azubuko <azubukouche@yahoo.com>
2022-11-17 05:25:28 +0100

    update: implemented post login screens

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="auth-wrapper">
+    <SideNav />
+    <TheHeader />
+    <div class="auth-pages">
+      <router-view />
+    </div>
   </div>
 </template>
 
+<script>
+import Vue from "vue";
+import SideNav from '@/components/layouts/SideNav.vue'
+import TheHeader from '@/components/layouts/TheHeader.vue'
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    SideNav,
+    TheHeader,
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: 'Rubik';
+  src: url('./assets/fonts/Rubik-Regular.ttf') format('truetype');
 }
 
-nav {
-  padding: 30px;
+$rubik: 'Rubik', sans-serif;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+body {
+  background-color: #121127;
+  font-family: Rubik, Helvetica, Arial, sans-serif;
+  color: #ffffff;
+  font-size: 16px;
+}
+
+img {
+  max-width: 100%;
+}
+
+a {
+  text-decoration: none;
+  display: inline-block;
+}
+
+ul,
+li {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.auth-wrapper {
+  display: flex;
+}
+
+.auth-pages {
+  padding: 77px 0 20px 0;
+  width: calc(100vw - 272px);
+  margin: 0 0 0 auto;
+
+  @media only screen and (max-width: 1100px) {
+    width: 100vw;
+    margin: 0 auto;
+    padding: 0px 0 20px 0;
   }
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,46 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'dashboard',
+    component: Dashboard
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/about-us',
+    name: 'about-us',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/points',
+    name: 'points',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Points.vue')
+  },
+  {
+    path: '/community',
+    name: 'community',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Community.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
   }
 ]
 
